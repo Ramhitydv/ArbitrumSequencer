3761ddd64cf1e25f38e6b8e48511e5162282c921
Ramhit <118090677+Ramhitydv@users.noreply.github.com>
2026-01-04 19:29:12 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/arbitrumsequencer/main.go
@@ -0,0 +1,23 @@
+// cmd/arbitrumsequencer/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "arbitrumsequencer/internal/arbitrumsequencer"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := arbitrumsequencer.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
